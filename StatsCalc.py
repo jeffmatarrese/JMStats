ad0d178f388b0cfeb28a094a5c7585b125c8d662
Jeff <jeff.matarrese@gmail.com>
2018-02-18 15:12:53 -0800

    Added min sample + ci for sample mean

--- StatsCalc.py
+++ StatsCalc.py
@@ -3,12 +3,12 @@
 def pointGivenConf(cLow, cHigh): #Find point estimate when given confidence interval
 	errorMargin = (cHigh - cLow)/2
 	pEstimate = cLow + errorMargin
-	print("The Error Margin is " + str(errorMargin))
+	print("The Error Margin is " + str(errorMargin)) #BUG: 
 	print("The point estimate is " + str(pEstimate))
 
 def zCrit(conf): #reminder to figure out scipy to use confidence intervals
 
-def confIntForPopProp(x, n, z): #figure out a more elegant way to just enter the confidence interval instead of z-crit!
+def ciPopProp(x, n, z): #figure out a more elegant way to just enter the confidence interval instead of z-crit!
 	p = x/n
 	q = 1-p
 	errorMargin = z * ((p*q)/n)**(1/2)
@@ -18,3 +18,14 @@ def confIntForPopProp(x, n, z): #figure out a more elegant way to just enter the
 	print("q = " + str(q))
 	print("The population proportion lies between " + str(cLow) + " and " + str(cHigh))
 	print("The margin of error is " + str(errorMargin))
+
+def ciSampleMean(xBar, n, stddev, z): #figure out a more elegant way to just enter the confidence interval instead of z-crit!
+	errorMargin = z * (stddev/n**(1/2))
+	cLow = xBar - errorMargin
+	cHigh = xBar + errorMargin
+	print("The sample mean lies between " + str(cLow) + " and " + str(cHigh))
+	print("The margin of error is " + str(errorMargin))
+
+def minSample(z, stddev, e):
+	n = ((z * stddev)/e)**2
+	print("The minimum sample size is " + str(n))
