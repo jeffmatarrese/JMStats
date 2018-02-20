cbb73a5eeebf91b9f63317d9fe0a62874eeea816
Jeff <jeff.matarrese@gmail.com>
2018-02-19 18:25:48 -0800

    Updated ci for when given p and minimum population for pop. proportion

--- StatsCalc.py
+++ StatsCalc.py
@@ -19,6 +19,14 @@ def ciPopProp(x, n, z): #figure out a more elegant way to just enter the confide
 	print("The population proportion lies between " + str(cLow) + " and " + str(cHigh))
 	print("The margin of error is " + str(errorMargin))
 
+def ciPopPropP(p, n, z): #Find confidence interval given p, n, and z
+	q = 1-p
+	errorMargin = z * ((p*q)/n)**(1/2)
+	cLow = p - errorMargin
+	cHigh = p + errorMargin
+	print("The population proportion lies between " + str(cLow) + " and " + str(cHigh))
+	print("The margin of error is " + str(errorMargin))
+
 def ciSampleMean(xBar, n, stddev, z): #figure out a more elegant way to just enter the confidence interval instead of z-crit!
 	errorMargin = z * (stddev/n**(1/2))
 	cLow = xBar - errorMargin
@@ -28,4 +36,8 @@ def ciSampleMean(xBar, n, stddev, z): #figure out a more elegant way to just ent
 
 def minSample(z, stddev, e):
 	n = ((z * stddev)/e)**2
+	print("The minimum sample size is " + str(n))
+
+def minSamplePop(p, z, e):
+	n = p * (1-p) * (z/e)**2
 	print("The minimum sample size is " + str(n))
