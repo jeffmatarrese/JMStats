44d3b08846680cbfe26f8336219fd5243dcc57a4
Jeff <jeff.matarrese@gmail.com>
2018-02-20 22:27:57 -0800

    General Cleanup + z-test start

--- StatsCalc.py
+++ StatsCalc.py
@@ -8,7 +8,7 @@ def pointGivenConf(cLow, cHigh): #Find point estimate when given confidence inte
 
 def zCrit(conf): #reminder to figure out scipy to use confidence intervals
 
-def ciPopProp(x, n, z): #figure out a more elegant way to just enter the confidence interval instead of z-crit!
+def ciPopProp(x, n, z): #Find confidence interval for population proportion
 	p = x/n
 	q = 1-p
 	errorMargin = z * ((p*q)/n)**(1/2)
@@ -19,7 +19,7 @@ def ciPopProp(x, n, z): #figure out a more elegant way to just enter the confide
 	print("The population proportion lies between " + str(cLow) + " and " + str(cHigh))
 	print("The margin of error is " + str(errorMargin))
 
-def ciPopPropP(p, n, z): #Find confidence interval given p, n, and z
+def ciPopPropP(p, n, z): #Find confidence interval for population proportion when given p
 	q = 1-p
 	errorMargin = z * ((p*q)/n)**(1/2)
 	cLow = p - errorMargin
@@ -27,17 +27,25 @@ def ciPopPropP(p, n, z): #Find confidence interval given p, n, and z
 	print("The population proportion lies between " + str(cLow) + " and " + str(cHigh))
 	print("The margin of error is " + str(errorMargin))
 
-def ciSampleMean(xBar, n, stddev, z): #figure out a more elegant way to just enter the confidence interval instead of z-crit!
+def ciSampleMean(xBar, n, stddev, z): #Find confidence interval for population mean
 	errorMargin = z * (stddev/n**(1/2))
 	cLow = xBar - errorMargin
 	cHigh = xBar + errorMargin
 	print("The sample mean lies between " + str(cLow) + " and " + str(cHigh))
 	print("The margin of error is " + str(errorMargin))
 
-def minSample(z, stddev, e):
+def minSample(z, stddev, e): #Minimum sample size given sample mean
 	n = ((z * stddev)/e)**2
 	print("The minimum sample size is " + str(n))
 
-def minSamplePop(p, z, e):
+def minSamplePop(p, z, e): #minimum sample size given population proportion
 	n = p * (1-p) * (z/e)**2
 	print("The minimum sample size is " + str(n))
+
+#Testing Testing Testing Testing
+
+def zTest(xBar, mean, stddev, n): #In Progress!
+	z = (xBar - mean)/(stddev / n**(1/2))
+	print("z = ")
+	testType = input("Is this a left-tail (lt), right-tail (rt), or two-tailed (tt) test?")
+	if test
